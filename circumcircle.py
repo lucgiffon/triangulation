b94e35908ae253590238309e70b4a8d2bedd9bda
Luc Giffon <luc.giffon@gmail.com>
2016-12-01 10:43:36 +0100

    just backup: big triangle not yet deleted, not commented, sometimes there is a bug concerning an edge, bad display

--- circumcircle.py
+++ circumcircle.py
@@ -1,52 +1,71 @@
 # code from http://codepad.org/KYyfy9CH
-from math import cos
-from math import sqrt
+import math
+import pylab as pl
+import matplotlib.pyplot as plt
+import numpy as np
+import random
 # Why math? Just 'cos
 
-# Distance between points A and B
-def distance(A, B):
-    n = len(A)
-    assert len(B) == n
-    return sum((A[i] - B[i]) ** 2 for i in range(n)) ** 0.5
+
+class CircumCircle:
+    def __init__(self, A, B, C):
+        self.__A = A
+        self.__B = B
+        self.__C = C
+
+        self.center = self.circumcenter(self.__A, self.__B, self.__C)
+        self.radius = self.circumradius(self.__A, self.__B, self.__C)
+
+    # Distance between points A and B
+    def distance(self, p1, p2):
+        n = len(p1)
+        assert len(p2) == n
+        return sum((p1[i] - p2[i]) ** 2 for i in range(n)) ** 0.5
+
+    # Cosine of angle p1-p2-p3
+    def cosine(self, p1, p2, p3):
+        d1, d2, d3 = self.distance(p2, p3), self.distance(p1, p3), self.distance(p1, p2)
+        return (d1 * d1 + d3 * d3 - d2 * d2) / (2 * d1 * d3)
 
 
-# Cosine of angle ABC
-def cosine(A, B, C):
-    a, b, c = distance(B, C), distance(A, C), distance(A, B)
-    return (a * a + c * c - b * b) / (2 * a * c)
+    # Cartesian coordinates of the point whose barycentric coordinates
+    # with respect to the triangle ABC are [p,q,r]
+    def barycentric(self, A, B, C, p, q, r):
+        n = len(A)
+        assert len(B) == len(C) == n
+        s = p + q + r
+        p, q, r = p / s, q / s, r / s
+        return tuple([p * A[i] + q * B[i] + r * C[i] for i in range(n)])
 
 
-# Cartesian coordinates of the point whose barycentric coordinates
-# with respect to the triangle ABC are [p,q,r]
-def barycentric(A, B, C, p, q, r):
-    n = len(A)
-    assert len(B) == len(C) == n
-    s = p + q + r
-    p, q, r = p / s, q / s, r / s
-    return tuple([p * A[i] + q * B[i] + r * C[i] for i in range(n)])
+    # Cartesian coordinates of the point whose trilinear coordinates
+    # with respect to the triangle ABC are [alpha,beta,gamma]
+    def trilinear(self, A, B, C, alpha, beta, gamma):
+        a = self.distance(B, C)
+        b = self.distance(A, C)
+        c = self.distance(A, B)
+        return self.barycentric(A, B, C, a * alpha, b * beta, c * gamma)
 
 
-# Cartesian coordinates of the point whose trilinear coordinates
-# with respect to the triangle ABC are [alpha,beta,gamma]
-def trilinear(A, B, C, alpha, beta, gamma):
-    a = distance(B, C)
-    b = distance(A, C)
-    c = distance(A, B)
-    return barycentric(A, B, C, a * alpha, b * beta, c * gamma)
+    # Cartesian coordinates of the circumcenter of triangle ABC
+    def circumcenter(self, A, B, C):
+        cosA = self.cosine(C, A, B)
+        cosB = self.cosine(A, B, C)
+        cosC = self.cosine(B, C, A)
+        return self.trilinear(A, B, C, cosA, cosB, cosC)
 
 
-# Cartesian coordinates of the circumcenter of triangle ABC
-def circumcenter(A, B, C):
-    cosA = cosine(C, A, B)
-    cosB = cosine(A, B, C)
-    cosC = cosine(B, C, A)
-    return trilinear(A, B, C, cosA, cosB, cosC)
+    def circumradius(self, A, B, C):
+        a = self.distance(B, C)
+        b = self.distance(A, C)
+        c = self.distance(A, B)
+        return (a * b * c)/math.sqrt((a+b+c) * (b+c-a) * (c+a-b) * (a+b-c))
+
+    def plot(self):
+        # plt.subplot(111).plot([self.__A[0], self.__B[0], self.__C[0], self.__A[0]], [self.__A[1], self.__B[1], self.__C[1], self.__A[1]])
+        plt.subplot(111).add_artist(plt.Circle(self.center, self.radius, fill=False))
+        # fig, ax = plt.subplots()
+        # c = plt.Circle(self.center, self.radius, fill=False)
+        # ax.add_artist(c)
 
 
-def radius(A, B, C):
-    a = distance(B, C)
-    b = distance(A, C)
-    c = distance(A, B)
-    return ((a * b * c)/sqrt((a+b+c) * (b+c-a) * (c+a-b) * (a+b-c)))
-
-# circumcenter((1, 2), (3, 3), (6, 0))
--- test.py
+++ test.py
@@ -3,9 +3,9 @@ import random
 
 import matplotlib.pyplot as plt
 import numpy as np
-import pylab as pl
 from matplotlib import collections as mc
 import networkx as nx
+import numpy
 
 import circumcircle
 
@@ -22,10 +22,9 @@ class Graph(nx.Graph):
 
 
     def plot(self):
-        pl.plot([node.x for node in self.nodes()], [node.y for node in self.nodes()], 'o')
+        plt.plot([node.x for node in self.nodes()], [node.y for node in self.nodes()], 'o')
         for e in self.edges():
-            pl.plot([e[0].x, e[1].x], [e[0].y, e[1].y])
-        pl.show()
+            plt.plot([e[0].x, e[1].x], [e[0].y, e[1].y])
 
     def test(self):
         for node in self.nodes_iter():
@@ -34,24 +33,47 @@ class Graph(nx.Graph):
 
     def legalize_edge(self, new_node, edge):
         print("\t\tEdge: ", " ".join([str(n.x) + "," + str(n.y) for n in edge]))
-        if self.is_edge_illegal(new_node, edge):
+        illegality = self.is_edge_illegal(new_node, edge)
+        if illegality is not None:
             # replace edge by new_node--k
-            #self.legalize_edge(new_node, edge[0]--k)
-            #self.legalize_edge(new_node, edge[1]--k)
-            pass
+            point_k = set(illegality.vertices).difference(set(edge)).pop()
+            self.remove_edge(edge[0], edge[1])
+            self.add_edge(new_node, point_k)
+            self.__tracker.add_edge(illegality, Triangle(point_k, new_node, edge[0]))
+            self.__tracker.add_edge(illegality, Triangle(point_k, new_node, edge[1]))
+            self.legalize_edge(new_node, [point_k, edge[0]])
+            self.legalize_edge(new_node, [point_k, edge[1]])
 
     def is_edge_illegal(self, new_node, edge):
         triangles = self.__tracker.get_triangles_containing_edge(edge)
         for tri in triangles:
             print("\t\tTested triangle: ", " ".join([str(n.x) + "," + str(n.y) for n in tri]))
-            circumcenter = circumcircle.circumcenter(*[(n.x, n.y) for n in tri])
-            radius = circumcircle.radius(*[(n.x, n.y) for n in tri])
-            print(circumcenter)
-            angle = random.randint(1, 360) * math.pi * 2
-            for i in range(0, 100):
-                pl.plot([math.cos(angle * i) * radius, math.sin(angle * i) * radius])
+            if new_node in tri.vertices:
+                pass
+            else:
 
-        pass
+                cc = circumcircle.CircumCircle((new_node.x, new_node.y), (edge[0].x, edge[0].y), (edge[1].x, edge[1].y))
+                plt.gca().set_aspect('equal', adjustable='box')
+                # cc.plot()
+                print("\t\t\tCircumcenter: %s - Radius: %s" % (cc.center, cc.radius))
+                quad_point = set(tri.vertices).difference(set(edge)).pop()
+                # plt.plot([quad_point.x, edge[0].x, edge[1].x, new_node.x],
+                #          [quad_point.y, edge[0].y, edge[1].y, new_node.y], color='red')
+                # plt.plot([new_node.x, edge[0].x, edge[1].x, new_node.x],
+                #          [new_node.y, edge[0].y, edge[1].y, new_node.y], color='green')
+                # plt.scatter(new_node.x, new_node.y, marker="o")
+                # plt.scatter(quad_point.x, quad_point.y, marker="+")
+                # plt.scatter(tri[0].x, tri[0].y, marker="<")
+                # plt.scatter(tri[1].x, tri[1].y, marker="<")
+                # plt.scatter(tri[2].x, tri[2].y, marker="<")
+                print(quad_point.x, "-", quad_point.y)
+                print(cc.radius ** 2)
+                print((quad_point.x - cc.center[0]) ** 2 + (quad_point.y - cc.center[1]) ** 2)
+                # plt.show()
+                if (quad_point.x - cc.center[0]) ** 2 + (quad_point.y - cc.center[1]) ** 2 < cc.radius ** 2:
+                    return tri
+
+        return None
 
     def delaunay_triangulation(self):
 
@@ -63,42 +85,44 @@ class Graph(nx.Graph):
         p3 = Node(x=-3 * self.__bound, y=-3 * self.__bound)
         init_triangle = [p1, p2, p3]
 
+        plt.plot([p.x for p in init_triangle] + [p1.x], [p.y for p in init_triangle] + [p1.y])
+
         self.__tracker.add_node(Triangle(*init_triangle))
 
-        for node in self.nodes():
-            upper_triangle = self.__tracker.get_triangle_containing_node(node)
+        for new_node in self.nodes():
+            upper_triangle = self.__tracker.get_triangle_containing_node(new_node)
             print("Upper triangle: ", " ".join([str(n.x) + "," + str(n.y) for n in upper_triangle]))
-            for node_triangle in upper_triangle:
-                self.add_edge(node_triangle, node)
+            for node_upper_triangle in upper_triangle.vertices:
+                self.add_edge(node_upper_triangle, new_node)
 
             comb = [(0, 1), (1, 2), (0, 2)]
 
             for c in comb:
-                tri = Triangle(node, upper_triangle[c[0]], upper_triangle[c[1]])
-                print("\tNew triangle: ", " ".join([str(n.x) + "," + str(n.y) for n in tri]))
-                self.__tracker.add_node(tri)
-                self.__tracker.add_edge(upper_triangle, tri)
-                self.legalize_edge(node, [upper_triangle[c[0]], upper_triangle[c[1]]])
+                new_tri = Triangle(new_node, upper_triangle[c[0]], upper_triangle[c[1]])
+                print("\tNew triangle: ", " ".join([str(n.x) + "," + str(n.y) for n in new_tri.vertices]))
+                self.__tracker.add_node(new_tri)
+                self.__tracker.add_edge(upper_triangle, new_tri)
+                self.legalize_edge(new_node, [upper_triangle[c[0]], upper_triangle[c[1]]])
 
 
 class Triangle:
     def __init__(self, p1, p2, p3):
-        self.nodes = [p1, p2, p3]
+        self.vertices = [p1, p2, p3]
 
     def __getitem__(self, item):
-        return self.nodes[item]
+        return self.vertices[item]
 
-    def __iter(self):
-        return self.nodes
+    def __iter__(self):
+        return iter(self.vertices)
 
     def contains(self, node):
         def sign(p1, p2, p3):
             return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y)
 
         # verifier que ca marche pour les noeuds qui sont sur une arrete
-        b1 = sign(node, self.nodes[0], self.nodes[1]) < 0.0
-        b2 = sign(node, self.nodes[1], self.nodes[2]) < 0.0
-        b3 = sign(node, self.nodes[2], self.nodes[0]) < 0.0
+        b1 = sign(node, self.vertices[0], self.vertices[1]) < 0.0
+        b2 = sign(node, self.vertices[1], self.vertices[2]) < 0.0
+        b3 = sign(node, self.vertices[2], self.vertices[0]) < 0.0
 
         return (b1 == b2) and (b2 == b3)
 
@@ -121,54 +145,52 @@ class TriangleTracker(nx.DiGraph):
     def get_triangles_containing_edge(self, edge):
         triangles = []
         for triangle in (x for x in self.nodes_iter() if self.out_degree(x)==0):
-            if set(edge).issubset(set(triangle)):
+            if set(edge).issubset(set(triangle.vertices)):
                 triangles.append(triangle)
         return triangles
 
 
+# angle = random.randint(1, 360) * math.pi * 2
+# for i in range(0, 100):
+#     pl.plot([math.cos(angle * i) * 50, math.sin(angle * i) * 50], 'o')
 
-N = 5
+
+N = 10
 BOUND = 100
+
+plt.subplot(111).set_xlim([-3 * BOUND * 1.5, 3 * BOUND * 1.5])
+plt.subplot(111).set_ylim([-3 * BOUND * 1.5, 3 * BOUND * 1.5])
+plt.gca().set_aspect('equal', adjustable='box')
+
 triangulation = Graph(bound=BOUND)
+# x,y = numpy.array(numpy.random.standard_normal((2,N)))
+x, y = np.array(range(N)), np.array(range(N))
+
 for i in range(N):
-    new_node = Node(random.uniform(-BOUND, BOUND), random.uniform(-BOUND, BOUND))
+    x[i] = random.uniform(-BOUND, BOUND)
+    y[i] = random.uniform(-BOUND, BOUND)
+    new_node = Node(x[i], y[i])
     triangulation.add_node(new_node)
 
 triangulation.delaunay_triangulation()
 triangulation.plot()
 
-
-
-# # plt.scatter(x, y)
-# # plt.show()
-# import pylab as pl
-# from matplotlib import collections  as mc
-# lines = []
-# for i in range(N):
-#     for j in range(N):
-#             lines.append([(x[i], y[i]), (x[j], y[j])])
-# lc = mc.LineCollection(lines, linewidths=2)
-# fig, ax = pl.subplots()
-# ax.add_collection(lc)
-# ax.autoscale()
-# ax.margins(0.1)
-# plt.show()
+plt.show()
 
 import matplotlib.delaunay as triang
 import pylab
-import numpy
 
 # 10 random points (x,y) in the plane
-x,y =  numpy.array(numpy.random.standard_normal((2,10)))
+
 cens,edg,tri,neig = triang.delaunay(x,y)
 for t in tri:
    # t[0], t[1], t[2] are the points indexes of the triangle
    t_i = [t[0], t[1], t[2], t[0]]
    # print(x[t_i])
-   pylab.plot(x[t_i], y[t_i])
+   plt.plot(x[t_i], y[t_i])
 
-pylab.plot(x, y, 'o')
-pylab.show()
+plt.plot(x, y, 'o')
+plt.show()
 
 # define set of point P (number N) -> graph (point = Node)
 # define triangulation (= set of edges)
